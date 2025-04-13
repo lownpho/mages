8791ac08285ca9cf2f362c6c9a55caf167577851
Dario Greto <dario.greto@gmail.com>
2025-04-13 21:11:01 +0100

    Add player that moves around

---
+++ characters/player/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	var direction := Vector2(direction_x, direction_y).normalized()
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
