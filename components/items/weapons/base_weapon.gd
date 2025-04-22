328a51cb67868cb14f25459e05a249f4fe3ae6d8
Dario Greto <dario.greto@gmail.com>
2025-04-22 22:31:59 +0100

    Refactor weapon for reuse

---
+++ components/items/weapons/base_weapon.gd
@@ -0,0 +1,42 @@
+extends Node2D
+class_name BaseWeapon
+
+@export var bullet_scene: PackedScene
+@export var bullet_config: BulletConfig
+@export var fire_cooldown: float = 0.5
+@export var skill_scaling: float = 1
+@export var mana_cost: int = 1
+
+var can_fire: bool = true
+var fire_timer: Timer
+
+func _ready() -> void:
+	fire_timer = Timer.new()
+	fire_timer.one_shot = true
+	fire_timer.wait_time = fire_cooldown
+	fire_timer.timeout.connect(_on_fire_timer_timeout)
+	add_child(fire_timer)
+
+func spawn_bullet(direction: Vector2, skill: int) -> void:
+	var bullet = bullet_scene.instantiate()
+	configure_bullet(bullet, skill)
+	bullet.position = global_position
+	bullet.direction = direction
+	get_tree().root.add_child(bullet)
+
+func configure_bullet(bullet: Node2D, skill: int) -> void:
+	if not bullet_config:
+		print("No bullet configuration set!")
+		return
+		
+	bullet.damage = bullet_config.damage + skill_scaling * skill
+	bullet.distance = bullet_config.distance
+	bullet.speed = bullet_config.speed
+	bullet.collision_layer = bullet_config.collision_layer
+
+func _on_fire_timer_timeout() -> void:
+	can_fire = true
+
+func set_cooldown(time: float) -> void:
+	fire_cooldown = time
+	fire_timer.wait_time = time
--- components/items/weapons/one_bullet_weapon.gd
+++ components/items/weapons/one_bullet_weapon.gd
@@ -1,48 +1,12 @@
-extends Node2D
+extends BaseWeapon
 
-@export var bullet_scene: PackedScene
-@export var bullet_config: BulletConfig
-@export var fire_cooldown: float = 0.5
-@export var skill_scaling: float = 1
-@export var mana_cost: int = 1
-
-var can_fire: bool = true
-var fire_timer: Timer
-
-func _ready() -> void:
-	fire_timer = Timer.new()
-	fire_timer.one_shot = true
-	fire_timer.wait_time = fire_cooldown
-	fire_timer.timeout.connect(_on_fire_timer_timeout)
-	add_child(fire_timer)
+# Someone would argue that this is a RingBulletWeapon with a single bullet
+# And they would be right
 
 func fire(direction: Vector2, skill: int) -> void:
 	if not can_fire or not bullet_scene:
 		return
 		
-	var bullet = bullet_scene.instantiate()
-	configure_bullet(bullet, skill)
-	bullet.position = global_position
-	bullet.direction = direction
-	
-	get_tree().root.add_child(bullet)
-
+	spawn_bullet(direction, skill)
 	can_fire = false
 	fire_timer.start()
-
-func configure_bullet(bullet: Node2D, skill: int) -> void:
-	if not bullet_config:
-		print("No bullet configuration set!")
-		return
-		
-	bullet.damage = bullet_config.damage + skill_scaling * skill
-	bullet.distance = bullet_config.distance
-	bullet.speed = bullet_config.speed
-	bullet.collision_layer = bullet_config.collision_layer
-
-func _on_fire_timer_timeout() -> void:
-	can_fire = true
-
-func set_cooldown(time: float) -> void:
-	fire_cooldown = time
-	fire_timer.wait_time = time
