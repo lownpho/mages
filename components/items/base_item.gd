557cad58272f89bafdd929136c1cb30fda17fa83
Dario Greto <dario.greto@gmail.com>
2025-06-01 22:58:00 +0100

    base_item fixup

--- components/items/base_item.gd
+++ components/items/base_item.gd
@@ -7,13 +7,18 @@ class_name BaseItem
 @export var max_mana_modifier: int = 0
 
 func apply_stats(target) -> void:
-	target.max_health += max_health_modifier
-	target.max_mana += max_mana_modifier
-	target.skill += skill_modifier
-	target.speed += speed_modifier
+    target.max_health += max_health_modifier
+    target.max_mana += max_mana_modifier
+    target.skill += skill_modifier
+    target.speed += speed_modifier
+    target.health = clamp(target.health, 0, target.max_health)
+    target.mana = clamp(target.mana, 0, target.max_mana)
 
 func remove_stats(target) -> void:
-	target.max_health -= max_health_modifier
-	target.max_mana -= max_mana_modifier
-	target.skill -= skill_modifier
-	target.speed -= speed_modifier
+    target.max_health -= max_health_modifier
+    target.max_mana -= max_mana_modifier
+    target.skill -= skill_modifier
+    target.speed -= speed_modifier
+    target.health = clamp(target.health, 0, target.max_health)
+    target.mana = clamp(target.mana, 0, target.max_mana)
+
